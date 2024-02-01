ee2f169ca3f245874d5fcf80d692a885456dc8c9
RomanKhorol <Rom4uk691@gmail.com>
2024-02-02 00:18:55 +0200

    promicesAsynkAwaitAndScoupOfVar

---
+++ pockemon.js
@@ -0,0 +1,34 @@
+const insertToDOM = (pokemon) => {
+  const body = document.querySelector("body");
+  const image = document.createElement("img");
+  image.src = pokemon.sprites.front_default;
+  body.appendChild(image);
+};
+
+const sendReq = (id) => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        .then((responce) => responce.json())
+        .then((responce) => res(responce));
+    }, 2000);
+  });
+};
+
+(async () => {
+  try {
+    const responce1 = await sendReq(1);
+    insertToDOM(responce1);
+    console.log(responce1);
+
+    const responce2 = await sendReq(2);
+    insertToDOM(responce2);
+    console.log(responce2);
+
+    const responce3 = await sendReq(3);
+    insertToDOM(responce3);
+    console.log(responce3);
+  } catch (error) {
+    console.log(error);
+  }
+})();
