4fc9d2f05bc80e6c315755e66979451c3e68fa12
Roman Gordiienko <rom4uk691@gmail.com>
2024-02-05 16:07:38 +0200

    Delete func

--- scoups.js
+++ scoups.js
@@ -10,15 +10,3 @@ for (var i = 0; i < 10; i++) {
     a
   );
 }
-for (var i = 0; i < 10; i++) {
-  function num(i) {
-    console.log(i);
-  }
-  setTimeout(
-    (a) => {
-      num(i);
-    },
-    0,
-    a
-  );
-}
